39976c23d2725f9ad60a7b55aa7a8aa5ce04ae34
vallifanov <vallifanov@gmail.com>
2022-04-20 23:51:12 +1200

    fixed 100% coverage

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -2,7 +2,7 @@ package ru.netology.statistic;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class StatisticsServiceTest {
 
@@ -10,8 +10,8 @@ public class StatisticsServiceTest {
     void findMax() {
         StatisticsService service = new StatisticsService();
 
-        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-        long expected = 12;
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 20, 8, 6, 11, 11, 12};
+        long expected = 20;
 
         long actual = service.findMax(incomesInBillions);
 
